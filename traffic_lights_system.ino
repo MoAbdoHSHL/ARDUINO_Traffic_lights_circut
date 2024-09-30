7ab9d54bc9dce1bf0e5870698c447d47a02f7291
Mohamed Abdo <167376441+MoAbdoHSHL@users.noreply.github.com>
2024-09-30 09:15:54 +0200

    Update traffic_lights_system.ino

--- traffic_lights_system.ino
+++ traffic_lights_system.ino
@@ -1,57 +1,52 @@
-// C++ code
-//
-// BY MOHAMED ABDO
-//
+// C++ 
+//By Mohamed Abdo
 void setup()
 {
-  pinMode(12, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(12, OUTPUT); // Red LED
+  pinMode(10, OUTPUT); // Yellow LED
+  pinMode(8, OUTPUT);  // Green LED
 }
 
 void loop()
 {
+  // Red on, others off
   digitalWrite(12, HIGH);
   digitalWrite(10, LOW);
   digitalWrite(8, LOW);
-  delay(3000); // Wait for 3000 millisecond(s)
+  delay(3000); // Wait for 3000 milliseconds (3 seconds)
+
+  // Yellow on, others off
   digitalWrite(12, LOW);
   digitalWrite(10, HIGH);
   digitalWrite(8, LOW);
-  delay(500); // Wait for 500 millisecond(s)
-  digitalWrite(12, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(500); // Wait for 500 millisecond(s)
-  digitalWrite(12, LOW);
-  digitalWrite(10, HIGH);
-  digitalWrite(8, LOW);
-  delay(500); // Wait for 500 millisecond(s)
-  digitalWrite(12, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(500); // Wait for 500 millisecond(s)
-  digitalWrite(12, LOW);
-  digitalWrite(10, HIGH);
-  digitalWrite(8, LOW);
-  delay(500); // Wait for 500 millisecond(s)
-  digitalWrite(12, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
-  delay(500); // Wait for 500 millisecond(s)
+  delay(350); 
+
+  // Yellow light blinking pattern (3 times)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(10, HIGH);
+    delay(500); 
+    digitalWrite(10, LOW);
+    delay(500); 
+  }
+
+  // Green on, others off
   digitalWrite(12, LOW);
   digitalWrite(10, LOW);
   digitalWrite(8, HIGH);
-  delay(3000); // Wait for 3000 millisecond(s)
+  delay(3000); 
+
+  // Reset all lights before restarting the loop
   digitalWrite(12, LOW);
   digitalWrite(10, LOW);
   digitalWrite(8, LOW);
-  delay(500000); // Wait for 500000 millisecond(s)
-  digitalWrite(12, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(8, HIGH);
-  delay(500); // Wait for 500 millisecond(s)
+  
+  
+  // Yellow light blinking pattern (3 times)
+  for (int i = 0; i < 3; i++) {
+    digitalWrite(10, HIGH);
+    delay(500); 
+    digitalWrite(10, LOW);
+    delay(500); 
+  }
 }
-//
-// BY MOHAMED ABDO
-//
+//By Mohamed Abdo
