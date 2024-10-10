afc21147e0816695e67141fd6875ae2c091393c4
Mohamed Abdo <167376441+MoAbdoHSHL@users.noreply.github.com>
2024-10-10 15:43:35 +0200

    Update traffic_lights_system.ino

--- traffic_lights_system.ino
+++ traffic_lights_system.ino
@@ -1,52 +1,24 @@
-// C++ 
-//By Mohamed Abdo
 void setup()
 {
-  pinMode(12, OUTPUT); // Red LED
-  pinMode(10, OUTPUT); // Yellow LED
-  pinMode(8, OUTPUT);  // Green LED
+  pinMode(12, OUTPUT); // Red
+  pinMode(10, OUTPUT); // Yellow
+  pinMode(8, OUTPUT);  // Green
 }
-
-void loop()
-{
-  // Red on, others off
+void loop(){
   digitalWrite(12, HIGH);
   digitalWrite(10, LOW);
   digitalWrite(8, LOW);
-  delay(3000); // Wait for 3000 milliseconds (3 seconds)
-
-  // Yellow on, others off
+  delay(20000);
   digitalWrite(12, LOW);
   digitalWrite(10, HIGH);
   digitalWrite(8, LOW);
-  delay(350); 
-
-  // Yellow light blinking pattern (3 times)
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(10, HIGH);
-    delay(500); 
-    digitalWrite(10, LOW);
-    delay(500); 
-  }
-
-  // Green on, others off
+  delay(5000);
   digitalWrite(12, LOW);
   digitalWrite(10, LOW);
   digitalWrite(8, HIGH);
-  delay(3000); 
-
-  // Reset all lights before restarting the loop
+  delay(20000);
   digitalWrite(12, LOW);
-  digitalWrite(10, LOW);
+  digitalWrite(10, HIGH);
   digitalWrite(8, LOW);
-  
-  
-  // Yellow light blinking pattern (3 times)
-  for (int i = 0; i < 3; i++) {
-    digitalWrite(10, HIGH);
-    delay(500); 
-    digitalWrite(10, LOW);
-    delay(500); 
-  }
-}
-//By Mohamed Abdo
+  delay(5000);}
+//By: MOHAMED ABDO
